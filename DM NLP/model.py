694bce3e1a385d1eaab7e2b1c78104186936dfc0
Jim-King <jiminwan@buffalo.edu>
2019-08-29 19:38:18 -0400

    Update

--- DM NLP/model.py
+++ DM NLP/model.py
@@ -13,25 +13,7 @@ from ELMo import ElmoEmbeddingLayer
 epochs = 50
 Model_DIR = '/path/to/your/main/directory/'
 
-
-def tag_dataset(dataset, model_path):
-    model = load_model(model_path)
-    correctLabels = []
-    predLabels = []
-    b = Progbar(len(dataset))
-    for i, data in enumerate(dataset):
-        tokens, labels = data
-        tokens = np.asarray([tokens])
-        # char = np.asarray([char])
-        pred = model.predict([tokens], verbose=False)[0]
-        pred = pred.argmax(axis=-1)  # Predict the classes
-        correctLabels.append(labels)
-        predLabels.append(pred)
-        b.update(i)
-    b.update(i + 1)
-    return predLabels, correctLabels
-
-#
+## Build training dataset
 trainSentences = readfile("/path/to/your/CoNLL-2003/format/training/dataset")
 
 trainSentences = addCharInformatioin(trainSentences)
@@ -92,38 +74,47 @@ np.save("outputs/char2Idx.npy", char2Idx)
 
 train_batch, train_batch_len = createBatches(train_set)
 
+
+## Construct LSTM model now
+
+## Word Input
 words_input = Input(shape=(None,), dtype='int32', name='words_input')
+
+## Glove Embedding
 words = Embedding(input_dim=wordEmbeddings.shape[0], output_dim=wordEmbeddings.shape[1], weights=[wordEmbeddings],
                   trainable=False)(words_input)
 
+## Char input
 character_input = Input(shape=(None, 52), name='char_input')
+
+## Char Embedding
 embed_char_out = TimeDistributed(Embedding(input_dim=len(char2Idx), output_dim=50, embeddings_initializer=
                                  RandomUniform(minval=-0.5, maxval=0.5)), name='char_embedding')(character_input)
+
+##  Char LSTM
 char_lstm = TimeDistributed(Bidirectional(LSTM(25, return_sequences=False, return_state=False, recurrent_dropout=0.25)),
                             name='char_LSTM')(embed_char_out)
 
-#
+## Word ELMo Embedding
 words_elmo_input = Input(shape=(1,),  dtype='string', name='words_elmo_input')
 words_elmo = ElmoEmbeddingLayer(trainable=False)(words_elmo_input)
 
-
+## Concatenate all features
 output = concatenate([words, char_lstm, words_elmo], axis=-1)
 
+## word-BiLSTM
 output_lstm = Bidirectional(LSTM(100, return_sequences=True, dropout=0.50, recurrent_dropout=0.25))(output)
 
-# output_ffn = TimeDistributed(Dense(len(label2Idx), activation='softmax'))(output_lstm)
-
+##CRF output layer
 my_crf = CRF(len(label2Idx), sparse_target=True, name='CRF_layer')(output_lstm)
 
 model = Model(inputs=[words_input, character_input, words_elmo_input], outputs=[my_crf])
-
-# my_optimizer = optimizers.SGD(loss='sparse_categorical_crossentropy', optimizer='sgd', lr=0.15, decay=0.99)
-# model.compile(loss='sparse_categorical_crossentropy', optimizer='nadam')
 
 model.compile(optimizer='adam', loss=crf_loss, metrics=[crf_viterbi_accuracy])
 model.summary()
 
 
+## Train in epoches:
 for epoch in range(epochs):
     print("Epoch %d/%d" % (epoch, epochs))
     a = Progbar(len(train_batch_len))
@@ -136,9 +127,3 @@ for epoch in range(epochs):
 
 model.save(Model_DIR + "/outputs/DM_NLP_model.h5")
 
-
-#   Performance on test dataset
-# print("Test the trained model on the testing dataset:")
-# predLabels, correctLabels = tag_dataset(test_batch, str(Model_DIR + "model.h5"))
-# pre_test, rec_test, f1_test = compute_f1(predLabels, correctLabels, idx2Label)
-# print("Test-Data: Prec: %.3f, Rec: %.3f, F1: %.3f" % (pre_test, rec_test, f1_test))
